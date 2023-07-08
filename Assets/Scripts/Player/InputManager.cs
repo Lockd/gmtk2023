b7ae1895cc6bcca7787914346d27d22fdaf2b3a9
Lockd <pikagrigoriy@gmail.com>
2023-07-08 13:33:47 +0300

    Input system adjustments

--- Assets/Scripts/Player/InputManager.cs
+++ Assets/Scripts/Player/InputManager.cs
@@ -1,35 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] private InputAction interactAction;
     private CharacterMovement characterMovement;
+
+    private void OnEnable()
+    {
+        interactAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        interactAction.Disable();
+    }
 
     void Start()
     {
         characterMovement = GetComponent<CharacterMovement>();
-
+        interactAction.performed += _ => OnInteract();
     }
 
-    void FixedUpdate()
+    private void OnInteract()
     {
-        // Movement Logic
+        Inventory.Instance.OnPickUp();
+        characterMovement.OnEnterDoor();
+    }
+
+    private void FixedUpdate()
+    {
         float xDirection = Input.GetAxis("Horizontal");
         characterMovement.OnMove(xDirection);
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            // TODO this logic is hacky? 
-
-            // Pickup Logic
-            Inventory.Instance.OnPickUp();
-
-            // Door Logic
-            characterMovement.OnEnterDoor();
-        }
-
-        // TODO Hide logic
-        // TODO Ability logic
     }
 }
