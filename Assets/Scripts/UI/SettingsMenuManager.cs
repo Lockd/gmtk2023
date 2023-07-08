1a706dcbb7d004c893582dbd76356935a31b5a23
Vladislav Fedorov <vldslv.valeryevich@gmail.com>
2023-07-08 13:17:52 -0500

    Main Menu UI camera movement
    added camera movement in the main menu

--- Assets/Scripts/UI/SettingsMenuManager.cs
+++ Assets/Scripts/UI/SettingsMenuManager.cs
@@ -7,10 +7,18 @@ public class SettingsMenuManager : MonoBehaviour
 {
     public Button backButton;
 
-    public GameObject mainMenuObject;
+    public GameObject menuUI;
+
+    public GameObject menuPosition;
+
+    public Camera mainCamera;
+
+    MenuCameraMovement thisCamera;
     // Start is called before the first frame update
     void Start()
     {
+
+        thisCamera = mainCamera.GetComponent<MenuCameraMovement>();
         backButton.onClick.AddListener(ReturnToMainMenu);
     }
 
@@ -22,7 +30,8 @@ public class SettingsMenuManager : MonoBehaviour
 
     void ReturnToMainMenu()
     {
-        mainMenuObject.SetActive(true);
-        gameObject.SetActive(false);
+        thisCamera.isMoving = true;
+        thisCamera.moveTo = menuPosition;
+        thisCamera.direction = -1;
     }
 }
